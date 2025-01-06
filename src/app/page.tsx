be8e752db9e7d592e378049394c9aaf036c8d3ce
GracjanPW <gracjanwojciechowski2002@gmail.com>
2025-01-06 23:21:07 +0000

    01 project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link"
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-red-300">
       <p>Home</p>
+      <Link href={'/documents/124'}>Quick doc</Link>
     </div>
   )
 }
