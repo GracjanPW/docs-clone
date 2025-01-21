25c141a01247584af66c443a0acbc8ca228ed1f0
GracjanPW <gracjanwojciechowski2002@gmail.com>
2025-01-21 23:38:10 +0000

    13 Navigation bar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,19 +1,25 @@
 import { Editor } from "./editor";
+import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 
 interface DocumentIdPageProps {
-    params: Promise<{documentId:string}>;
+  params: Promise<{ documentId: string }>;
 }
 
-const DocumentIdPage = async ({params}:DocumentIdPageProps) => {
-    const {documentId} = await params;
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
 
-    return ( 
-        <div className="min-h-screen bg-[#fafbfd]">
-            <Toolbar/>
-            <Editor/>
-        </div>
-     );
-}
- 
+  return (
+    <div className="min-h-screen bg-[#fafbfd]">
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#fafbfd] print:hidden h-[112px]">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className="pt-[114px] print:pt-0">
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
 export default DocumentIdPage;
